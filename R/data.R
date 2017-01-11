3ddf014cfccbc1960852fc22a5879d374983589b
Telmo Brugnara <tbrugz@gmail.com>
2017-01-11 11:21:42 -0200

    doc: datasets documented

---
+++ R/data.R
@@ -0,0 +1,24 @@
+
+#' Municipalities population data from 2010 census.
+#'
+#' A \code{data.frame} with municipalities population data from 2010 census.
+#'
+"habitantes2010"
+
+#' Municipalities population estimates file sources
+#'
+#' A \code{data.frame} with municipalities population file sources by year. Internally
+#' used by \code{\link{populacao_municipios_download}} & \code{\link{populacao_municipios}}
+#'
+"ibge.populacao.sources"
+
+#' Mapping between IBGE's & TSE's municipalities codes
+#'
+"municipioIbgeTseMap"
+
+#' Series codes from \code{seriesestatisticas} website
+#'
+#' @seealso \url{http://seriesestatisticas.ibge.gov.br/}
+#'
+"seriesEstatisticas"
+
