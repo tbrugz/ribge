947ffd815651a6e41786f6ef0a527ebf11f1ef6b
Telmo Brugnara <tbrugz@gmail.com>
2017-06-22 16:32:00 -0300

    doc: updated Roxygen, better dataset docs

--- R/data.R
+++ R/data.R
@@ -1,24 +1,38 @@
 
+# https://stackoverflow.com/questions/2310409/how-can-i-document-data-sets-with-roxygen
+
 #' Municipalities population data from 2010 census.
 #'
 #' A \code{data.frame} with municipalities population data from 2010 census.
 #'
-"habitantes2010"
+#' @name habitantes2010
+#' @docType data
+#"habitantes2010"
+NULL
 
 #' Municipalities population estimates file sources
 #'
 #' A \code{data.frame} with municipalities population file sources by year. Internally
 #' used by \code{\link{populacao_municipios_download}} & \code{\link{populacao_municipios}}
 #'
-"ibge.populacao.sources"
+#' @name ibge.populacao.sources
+#' @docType data
+#"ibge.populacao.sources"
+NULL
 
 #' Mapping between IBGE's & TSE's municipalities codes
 #'
-"municipioIbgeTseMap"
+#' @name municipioIbgeTseMap
+#' @docType data
+#"municipioIbgeTseMap"
+NULL
 
 #' Series codes from \code{seriesestatisticas} website
 #'
 #' @seealso \url{http://seriesestatisticas.ibge.gov.br/}
 #'
-"seriesEstatisticas"
+#' @name seriesEstatisticas
+#' @docType data
+#"seriesEstatisticas"
+NULL
 
