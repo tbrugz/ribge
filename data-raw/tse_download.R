cb46d81790f11bcd9133621e67d2f710639a4a0b
Telmo Brugnara <tbrugz@gmail.com>
2021-08-09 21:26:07 -0300

    tse: download from 'perfil_eleitorado_ATUAL'

--- data-raw/tse_download.R
+++ data-raw/tse_download.R
@@ -43,6 +43,45 @@ tseMunicipios2020 <- tseMunicipios2020 %>%
 
 names(tseMunicipios2020) <- c("uf", "cod_municipio_tse", "nome_municipio_tse")
 
-#write_csv(tseMunicipios2020, file = "doc/tse-municipios.csv")
+##write_csv(tseMunicipios2020, file = "doc/tse-municipios.csv")
 
-#usethis::use_data(tseMunicipios2020, overwrite = TRUE)
+##usethis::use_data(tseMunicipios2020, overwrite = TRUE)
+
+####
+
+download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/perfil_eleitorado/perfil_eleitorado_ATUAL.zip", "perfil_eleitorado_ATUAL.zip");
+
+df <- readr::read_csv2(
+  #unz(description = "perfil_eleitorado_ATUAL.zip", filename = "perfil_eleitorado_ATUAL.csv"),
+  "perfil_eleitorado_ATUAL.head.csv",
+  locale = loc)
+
+zonas <- readr::read_csv2(
+  unz(description = "perfil_eleitorado_ATUAL.zip", filename = "perfil_eleitorado_ATUAL.csv"),
+  #"perfil_eleitorado_ATUAL.head.csv",
+  locale = loc) %>%
+  select("SG_UF","CD_MUNICIPIO","NM_MUNICIPIO","NR_ZONA") %>%
+  distinct()
+
+# SG_UF,CD_MUNICIPIO,NM_MUNICIPIO,NR_ZONA
+# "DT_GERACAO";"HH_GERACAO";"ANO_ELEICAO";"SG_UF";"CD_MUNICIPIO";"NM_MUNICIPIO";"CD_MUN_SIT_BIOMETRIA";"DS_MUN_SIT_BIOMETRIA";"NR_ZONA";"CD_GENERO";"DS_GENERO";"CD_ESTADO_CIVIL";"DS_ESTADO_CIVIL";"CD_FAIXA_ETARIA";"DS_FAIXA_ETARIA";"CD_GRAU_ESCOLARIDADE";"DS_GRAU_ESCOLARIDADE";"QT_ELEITORES_PERFIL";"QT_ELEITORES_BIOMETRIA";"QT_ELEITORES_DEFICIENCIA";"QT_ELEITORES_INC_NM_SOCIAL"
+
+names(zonas) <- c("uf", "cod_municipio_tse", "nome_municipio_tse", "nr_zona")
+
+zonas$cod_municipio_tse <- stringr::str_pad(zonas$cod_municipio_tse, 5, pad="0")
+
+zonas <- zonas %>%
+  arrange(cod_municipio_tse, nr_zona)
+
+tseMunicipios <- zonas %>%
+  #filter(uf != "ZZ") %>%
+  select(uf, cod_municipio_tse, nome_municipio_tse) %>%
+  distinct() %>%
+  arrange(cod_municipio_tse)
+
+#View(filter(zonas, uf == "ZZ"))
+
+#write_csv(tseMunicipios, file = "doc/tse-municipios.csv")
+#usethis::use_data(tseMunicipios, overwrite = TRUE)
+
+#write_csv(zonas, file = "doc/tse-zonas-eleitorais.csv")
